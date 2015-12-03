4413760acd80e7a8e3ab88aa244f5bf6450b226a
Trey Brister <webstandardcss@gmail.com>
2015-12-03 11:11:49 -0600

    new page notable results

--- results.php
+++ results.php
@@ -1,5 +1,5 @@
 <?php
-	$title = "Practice Areas"; 
+	$title = "Notable Results"; 
 	include_once('header.php'); 
 	include_once('header_bottom.php'); 
 ?>
@@ -8,68 +8,17 @@
 <section class="contents col-sm-8">
 <!-- Contents Starts -->
 
-    <h1>Practice areas</h1>
-    <h4><a href="Practice-Areas-15.php">Practice Areas</a></h4>
-    <p>A Texas Lawyer With A Strong Legal Background Serving Individuals and Business in Longview and the East Texas Region At Jose Sanchez Law Firm, P.C., we take a comprehensive approach to meeting your legal needs. We have handled a wide
-      <a href="Practice-Areas-15.php">
-        <strong>...</strong>
-      </a>
-    </p>
-    <h4><a href="Personal-Injury.php">Personal Injury</a></h4>
-    <p>If you have been injured in a car crash or workplace accident, the physical pain can be overwhelming. Add in the anxiety about time lost at work and the medical bills piling up, and you may start to lose hope. At Jose Sanchez Law Firm, P.C., we
-      <a href="Personal-Injury.php">
-        <strong>...</strong>
-      </a>
-    </p>
-    <h4><a href="Motor-Vehicle-Accidents.php">Motor Vehicle Accidents</a></h4>
-    <p>A car or truck accident can have a huge impact on your life. If you have sustained injuries because of someone else's negligence, you may be unable to work for months or even a year or more. Pain and physical disabilities can make you lose hope.
-      <a href="Motor-Vehicle-Accidents.php">
-        <strong>...</strong>
-      </a>
-    </p>
-    <h4><a href="Workplace-Injuries.php">Workplace Injuries</a></h4>
-    <p>A construction site accident or oil field injury can change your life in a split second. Pain, the loss of mobility and months of lost wages can cause emotional distress and a financial burden. Workplace injuries demand the skilled representation
-      <a href="Workplace-Injuries.php">
-        <strong>...</strong>
-      </a>
-    </p>
-    <h4><a href="Immigration.php">Immigration</a></h4>
-    <p>When you are trying to start a new life in an unfamiliar country, you need an attorney who has extensive knowledge of immigration law. Jose was born in Mexico and grew up in Longview. He went through the immigration process himself in 2003. The law
-      <a href="Immigration.php">
-        <strong>...</strong>
-      </a>
-    </p>
-    <h4><a href="Criminal-Defense.php">Criminal Defense</a></h4>
-    <p>When you are facing charges for DUI, drug crimes or assault, it may seem as if the odds are stacked against you. You need a lawyer on your side who you can trust to protect your rights. You can rest assured knowing that Jose Sanchez Law Firm, P.C.,
-      <a href="Criminal-Defense.php">
-        <strong>...</strong>
-      </a>
-    </p>
-    <h4><a href="DUI-Defense.php">DUI Defense</a></h4>
-    <p>The consequences for driving drunk can be far-reaching. You may lose your license, your green card or your freedom. You may face employment problems due to a criminal record and an inability to possess a driver's license. At Jose Sanchez Law Firm,
-      <a href="DUI-Defense.php">
-        <strong>...</strong>
-      </a>
-    </p>
-    <h4><a href="Drug-Crimes.php">Drug Crimes</a></h4>
-    <p>An arrest on drug possession charges can make you feel trapped. You may be facing thousands of dollars in fines, jail time and a damaging criminal record. You need a dedicated lawyer who will help protect your rights. Jose Sanchez Law Firm, P.C., in
-      <a href="Drug-Crimes.php">
-        <strong>...</strong>
-      </a>
-    </p>
-    <h4><a href="Assault-and-Violent-Crimes.php">Assault and Violent Crimes</a></h4>
-    <p>Assault and domestic violence charges can carry serious penalties. What began as an argument could lead to jail time and the loss of your parental rights. If you are not a U.S. citizen, an assault case could result in denial of citizenship or
-      <a href="Assault-and-Violent-Crimes.php">
-        <strong>...</strong>
-      </a>
-    </p>
+    <h1><?php echo $title ?></h1>
+    <h4><a href="#"><?php echo $title ?></a></h4>
+   <?php if(0) { /* start comment html
     <h4><a href="Civil-Litigation.php">Civil Litigation</a></h4>
     <p>Civil LitigationProtecting Your Interests When you do not speak English, you can feel like a target for companies or people to take advantage of. Whether you are facing a contract dispute or a denial of an insurance claim, you need an experienced
       <a href="Civil-Litigation.php">
         <strong>...</strong>
       </a>
     </p>
-    
+    /* end comment html */ } ?>
+
 <!-- Contents Ends -->
 </section>
 
