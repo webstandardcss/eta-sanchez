239751b710be4911aad213588b243a01d72d05a8
Trey Brister <webstandardcss@gmail.com>
2016-06-21 11:21:12 -0500

    header spanish

--- spanish/header.php
+++ spanish/header.php
@@ -26,6 +26,16 @@
      player.api_play();
    });
 </script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-79493901-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 <title><?php echo $title; ?></title>
 </head>
